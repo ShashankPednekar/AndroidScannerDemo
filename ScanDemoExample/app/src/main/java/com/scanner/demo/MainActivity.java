33ebd0186e3b62fa0f906a3fd404c823c034188b
Shashank <shashank.pednekar@creditmate.in>
2019-07-12 13:00:14 +0530

    refactor: upgraded to api 28

--- ScanDemoExample/app/src/main/java/com/scanner/demo/MainActivity.java
+++ ScanDemoExample/app/src/main/java/com/scanner/demo/MainActivity.java
@@ -7,12 +7,13 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.scanlibrary.ScanActivity;
 import com.scanlibrary.ScanConstants;
@@ -20,7 +21,7 @@ import com.scanlibrary.ScanConstants;
 import java.io.IOException;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_CODE = 99;
     private Button scanButton;
